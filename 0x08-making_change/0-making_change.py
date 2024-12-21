99643a658cfa59a83eedf7dac910b57b5aa364af
iceking-fct <miracleamajama14@gmail.com>
2024-12-21 11:55:50 +0100

    task updated

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,30 +2,30 @@
 """Coin Change problem"""
 
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed to
-    meet a given amount <total>.
+    """
+    Determines the fewest number of coins needed to meet a given amount <total>.
 
-    Params:
-       - coins(obj:list): a list of coin values available.
-         The value of a coin will always be an int > 0
-       - total(number): the total change to give.
+    Parameters:
+       - coins (list): List of coin values available.
+         The value of a coin will always be an int > 0.
+       - total (int): The total change to give.
 
     Assumes:
-       - There is an infinite number of coins for each denomination
-         in our list.
+       - There is an infinite number of coins for each denomination in the list.
+
     Returns:
-       - 0 if total is <= 0
-       - -1 if total cannot be met by any number of coins we have
+       - 0 if total is <= 0.
+       - -1 if total cannot be met by any number of coins.
+       - Minimum number of coins required otherwise.
     """
     if total <= 0:
-        return 0
-    if not coins:
-        return -1
+        return 0  # Handle case for total <= 0
 
-    # Initialize DP table
+    # Initialize DP array
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed for total of 0
+    dp[0] = 0  # Base case: 0 coins needed to make 0 total
 
+    # Fill DP array
     for coin in coins:
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
